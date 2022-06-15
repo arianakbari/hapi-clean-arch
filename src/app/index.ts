b5ccb338b076157d9c71f64a031f5582950ba673
Arian Akbari <akbariarian@gmail.com>
2022-06-15 05:43:09 +0430

    Added winston as a logger and also added a wildcard 404 route.

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,6 +1,7 @@
 import * as Hapi from "@hapi/hapi";
 import { registerRoutes } from "./routes";
 import { config } from "./config";
+import { Logger, LOG_LABELS } from "./helpers/logger";
 
 const init = async () => {
   const server = Hapi.server({
@@ -10,11 +11,12 @@ const init = async () => {
   registerRoutes(server);
 
   await server.start();
-  console.log("Server running on %s", server.info.uri);
+
+  Logger.info(LOG_LABELS.START_APP, `Server running on ${server.info.uri}`);
 };
 
 process.on("unhandledRejection", (err) => {
-  console.log(err);
+  Logger.error(LOG_LABELS.UNHANDLED_INTERNAL_ERROR, "Error", err as Error);
   process.exit(1);
 });
 
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -3,4 +3,15 @@ import { jobRoutes } from "./v1/JobRoutes";
 
 export function registerRoutes(server: Hapi.Server) {
   server.route(jobRoutes);
+  server.route({
+    method: "*",
+    path: "/{any*}",
+    handler: (request, h) =>
+      h
+        .response({
+          success: false,
+          message: "Url not found!",
+        })
+        .code(404),
+  });
 }
